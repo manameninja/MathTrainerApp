17e16a1342c8a6c7813c8e9088fa0dd00eab27ab
Daniil Pavlenko <danyy.pavl@gmail.com>
2023-04-01 19:13:37 +0300

    HomeWork * add shadows to 3 button and designed "back button"

--- MathTrainerApp/MathTrainerApp/TrainViewController.swift
+++ MathTrainerApp/MathTrainerApp/TrainViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 final class TrainViewController: UIViewController {
+    @IBOutlet var buttonsTrainerCollection: [UIButton]!
     // MARK: Properties
     var type: MathTypes = .add {
         didSet{
             print(type)
+        }
+    }
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurateButtonsTrainer()
+    }
+    
+    private func configurateButtonsTrainer() {
+        // Add shadow
+        buttonsTrainerCollection.forEach { button in
+            button.layer.shadowColor = UIColor.darkGray.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowOpacity = 0.4
+            button.layer.shadowRadius = 3
         }
     }
 }
